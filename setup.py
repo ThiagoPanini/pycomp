98bfc1ba8473c4df9ab2baa98c0a390227c05c05
ThiagoPanini <thipanini94@gmail.com>
2020-10-26 23:10:10 -0300

    [TESTE] Gerando .whl and tar.gz

--- setup.py
+++ setup.py
@@ -51,12 +51,7 @@ setup(
         'Intended Audience :: System Administrator',
         'Operation System :: OS Independent',
         'Topic :: Software Development',
-        'Environment :: Web Environment',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'License :: OSI Approved :: MIT License',
-    ]
+        'Programming Language :: Python :: 3',
+    ],
+    python_requires=">=3.8.6"
 )
