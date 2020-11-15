37bb726165fa60d56b720325d742574ff6edb13c
ThiagoPanini <thipanini94@gmail.com>
2020-11-14 22:25:05 -0300

    Incluindo testes realizados

---
+++ tests/ml/trainer_test.py
@@ -0,0 +1,69 @@
+# Importando módulo
+from pycomp.ml.trainer import ClassificadorBinario, plot_shap_analysis
+
+# Importando bibliotecas e lendo dados
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from warnings import filterwarnings
+filterwarnings('ignore')
+
+raw_data = pd.read_csv('tests/ml/titanic_data/titanic.csv')
+raw_data.columns = [col.lower().strip() for col in raw_data.columns]
+raw_data = raw_data.loc[:, ['survived', 'pclass', 'age', 'sibsp', 'fare']]
+raw_data.dropna(inplace=True)
+
+# Separando X e y
+X = raw_data.drop('survived', axis=1)
+y = raw_data['survived'].values
+
+# Separando em treino e teste
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=.20, random_state=42)
+features = list(X_train.columns)
+
+# Preparando classificadores
+tree_clf = DecisionTreeClassifier()
+log_reg = LogisticRegression()
+forest_clf = RandomForestClassifier()
+
+set_classifiers = {
+    'DecisionTree': {
+        'model': tree_clf,
+        'params': None
+    },
+    'LogisticRegression': {
+        'model': log_reg,
+        'params': None
+    },
+    'RandomForest': {
+        'model': forest_clf,
+        'params': None
+    }
+}
+
+# Variáveis de execução
+OUTPUT_PATH = 'tests/ml/output'
+
+# Inicializando objeto
+trainer = ClassificadorBinario()
+
+# Fluxo de treino
+trainer.training_flow(set_classifiers, X_train, y_train, X_test, y_test, features, output_path=OUTPUT_PATH)
+
+# Análise gŕafica
+trainer.visual_analysis(features=features, model_shap='DecisionTree', output_path=OUTPUT_PATH)
+
+# Retornando informações relevantes de um modelo específico
+model = trainer._get_estimator(model_name='RandomForest')
+metrics = trainer._get_metrics(model_name='RandomForest')
+model_info = trainer._get_model_info(model_name='RandomForest')
+
+
+"""
+Ideias: salvar o .pkl dos modelos (nova pasta chamada models com os .pkl) [DONE]
+Modificar gráficos pra sempre plotar visões pra todos os modelos presentes na classe [DONE]
+
+Criar método para plotagem gráfica dos scores [DONE]
+"""
