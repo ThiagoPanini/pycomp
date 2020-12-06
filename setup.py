104bfcc32765beb6e509991dbf205e3906fb0f0d
ThiagoPanini <thipanini94@gmail.com>
2020-12-06 17:15:21 -0300

    Release v0.1.1

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ reqs = [str(ir.req) for ir in install_reqs]"""
 # Criando setup
 setup(
     name='pycomp',
-    version='0.1.0',
+    version='0.1.1',
     author='Thiago Panini',
     author_email='thipanini94@gmail.com',
     packages=find_packages(),
